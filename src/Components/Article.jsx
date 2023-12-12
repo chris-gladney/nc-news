79800ee10fb27b57c7f11f910e1f4cd588fa1820
Christopher Gladney <christophergladney282@gmail.com>
2023-12-12 11:26:49 +0000

    Ticket 4 for review

---
+++ src/Components/Article.jsx
@@ -0,0 +1,36 @@
+function Article({
+  title,
+  topic,
+  author,
+  body,
+  created_at,
+  votes,
+  article_image_url,
+  comment_count,
+}) {
+
+    {console.log(article_image_url)}
+  return (
+    <div className="article">
+        
+      <h4>{title}</h4>
+      {article_image_url ? (
+        <img src={article_image_url} alt={`image by ${author}`} />
+      ) : null}
+      <p>{body}</p>
+      <p>{author}</p>
+      <p>Topic: {topic}</p>
+      <p>Date: {created_at}</p>
+      <div className="votes">
+        <h5>Votes</h5>
+        <p>{votes}</p>
+      </div>
+      <div className="comments">
+        <button>Show Comments</button>
+        <p>{comment_count}</p>
+      </div>
+    </div>
+  );
+}
+
+export default Article;
