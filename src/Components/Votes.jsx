afec0849343b48746e6ae98dd27844e712a43bf8
Christopher Gladney <christophergladney282@gmail.com>
2024-02-02 12:22:52 +0000

    Fixed source for up and down vote images

--- src/Components/Votes.jsx
+++ src/Components/Votes.jsx
@@ -23,7 +23,7 @@ function Votes({
       <div className="votes-container">
         <img
           className={`vote ${upVote ? "clicked-vote" : "unclicked-vote"}`}
-          src={"../../images/upvote.png"}
+          src={`${__dirname}../../images/upvote.png`}
           onClick={() => {
             patchVotes(article_id, upVote, downVote, displayedVotes, setDisplayedVotes, true);
             if (upVote === false) {
@@ -37,7 +37,7 @@ function Votes({
         />
         <img
           className={`vote ${downVote ? "clicked-vote" : "unclicked-vote"}`}
-          src={"../../images/downvote.png"}
+          src={`${__dirname}../../images/downvote.png`}
           onClick={() => {
             patchVotes(article_id, upVote, downVote, displayedVotes, setDisplayedVotes, false);
             if (downVote === false) {
